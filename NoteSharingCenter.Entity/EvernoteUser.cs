65c8d45547a662f6ae076805ac0dff6ac49a9cb3
Gurhan <gurhangedik@hotmail.com>
2019-10-25 00:08:30 +0300

    Update 2
    Tasarımda değişiklik yapıldı
    katmanlar oluşturuldu
    codefirst için tablollar oluşturuldu
    Entity kuruldu

---
+++ NoteSharingCenter.Entity/EvernoteUser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteSharingCenter.Entity
+{
+    [Table("EvernoteUsers")]
+    public class EvernoteUser:MyEntityBase
+    {
+        [StringLength(30)]
+        public string Name { get; set; }
+
+        [StringLength(30)]
+        public string Surname { get; set; }
+
+        [Required, StringLength(30)]
+        public string Username { get; set; }
+
+        [Required, StringLength(50)]
+        public string Email { get; set; }
+
+        [Required, StringLength(30)]
+        public string Password { get; set; }
+
+        [Required]
+        public Guid ActiveteGuid { get; set; }
+
+        public bool IsActive { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public virtual List<Note> Notes { get; set; }
+        public virtual List<Comment> Comments { get; set; }
+        public virtual List<Liked> Likes { get; set; }
+    }
+}
---
+++ NoteSharingCenter.Entity/Note.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteSharingCenter.Entity
+{
+    [Table("Notes")]
+    public class Note:MyEntityBase
+    {
+        [Required, StringLength(100)]
+        public string Title { get; set; }
+
+        [Required, StringLength(5000)]
+        public string Text { get; set; }
+
+        public bool IsDraft { get; set; }
+        public int LikeCount { get; set; }
+        public int CategoryId { get; set; }
+
+        public virtual EvernoteUser Owner { get; set; }
+        public virtual List<Comment> Comments { get; set; }
+        public virtual List<Category> Category { get; set; }
+        public virtual List<Liked> Likes { get; set; }
+    }
+}
